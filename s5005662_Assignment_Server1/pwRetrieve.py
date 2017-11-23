708520da65405b5d47e8883e00e8f2ae636b390e
Lewis <lewisawilkinson@outlook.com>
2017-11-23 20:51:50 +0000

    Test

--- s5005662_Assignment_Server1/pwRetrieve.py
+++ s5005662_Assignment_Server1/pwRetrieve.py
@@ -12,10 +12,12 @@ def menu_input():
 
 
 def keyword_search():
+    print ""
     return raw_input("Enter keyword to search by: ")
 
 
 def encoding_type():
+    print ""
     return raw_input("Enter the decoding style: ")
 
 
@@ -40,15 +42,15 @@ def search_files(keyword, type_encoding):
             current_file.close()
             if keyword in decoded_text:
                 all_text = decoded_text
-                print "The total number of files searched through is: {}".format(total_file_count)
+                print ""
                 search_result(pw_file_count, decoded_text, keyword)
-            # else:
-               #  print "No file has been found with that keyword"
-
+        if decoded_text == "":
+            print "No file has been found with that keyword"
+    print "The total number of files searched through is: {}".format(total_file_count)
 
 def search_result(pwf_count, text, kw):
-    print""
     print "The number of files searched through to find the password was: {}".format(pwf_count)
+    print""
     print "The text in the file is: {}".format(text)
     split_text(text, kw)
 
