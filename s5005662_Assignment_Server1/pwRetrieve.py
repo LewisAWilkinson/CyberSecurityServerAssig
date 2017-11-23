18c9f629f8466e2e4eb5a89a9cfef50a6260c488
Lewis <lewisawilkinson@outlook.com>
2017-11-23 01:16:48 +0000

    Test

--- s5005662_Assignment_Server1/pwRetrieve.py
+++ s5005662_Assignment_Server1/pwRetrieve.py
@@ -2,11 +2,27 @@ import os
 import matplotlib
 
 
-def menu():
-    menu_option = raw_input("Input menu option: ")
+def menu_input():
+    print("")
+    print("Menu Options:")
+    print("1: Search through files to find a keyword")
+    print("2: Display a histogram of the data found")
+    print("")
+    return raw_input("Input menu select: ")
 
 
-def search_files():
+def selection(user_input):
+    if user_input == "1":
+        search_files(keyword_search())
+    else:
+        print "Invalid input"
+
+
+def keyword_search():
+    return raw_input("Enter keyword to search by: ")
+
+
+def search_files(keyword):
     pw_file_count = 0
     total_file_count = 0
     for root, dirs, files in os.walk("received_files/documents"):
@@ -18,16 +34,17 @@ def search_files():
             encoded_text = current_file.read()
             decoded_text = encoded_text.decode(encoding="Hex")
             current_file.close()
-            if "Millenium2000" in decoded_text:
-                password = decoded_text
+            if keyword in decoded_text:
+                all_text = decoded_text
                 print "The number of files searched through to find the password was: {}".format(pw_file_count)
     print "The total number of files searched through is: {}".format(total_file_count)
-    return password
+    print "The text from the file found was: {}",format(all_text)
+    return all_text
+    pw_text = all_text
+
 
 # def split_text():
 
+selection(menu_input())
 
 
-print search_files()
-
-menu()
