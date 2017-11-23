d5d31264f4b0b4cdce8806f5c0a0c4be9e6440ce
Lewis <lewisawilkinson@outlook.com>
2017-11-23 00:13:02 +0000

    Test

--- s5005662_Assignment_Server1/pwRetrieve.py
+++ s5005662_Assignment_Server1/pwRetrieve.py
@@ -1,24 +1,33 @@
 import os
 import matplotlib
 
+
 def menu():
-    print("")
+    menu_option = raw_input("Input menu option: ")
 
 
 def search_files():
+    pw_file_count = 0
+    total_file_count = 0
     for root, dirs, files in os.walk("received_files/documents"):
         for eachfile in files:
             path = root + "/" + eachfile
+            pw_file_count += 1
+            total_file_count += 1
             current_file = open(path, "r")
             encoded_text = current_file.read()
             decoded_text = encoded_text.decode(encoding="Hex")
             current_file.close()
             if "Millenium2000" in decoded_text:
-                return decoded_text
+                password = decoded_text
+                print "The number of files searched through to find the password was: {}".format(pw_file_count)
+    print "The total number of files searched through is: {}".format(total_file_count)
+    return password
+
+# def split_text():
 
 
-def split_text():
 
+print search_files()
 
-
-print searchfiles()
+menu()
