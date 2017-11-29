0c9c9a9b9ef2fdf850dab5933f99e46641bb06bb
Lewis <lewisawilkinson@outlook.com>
2017-11-29 23:12:12 +0000

    File changes

--- s5005662_Assignment_Server1/unzip.py
+++
@@ -1,6 +0,0 @@
-import zipfile as zfile
-
-
-def unzip_secret(pw):  # Unzips the file secret.zip using the password found using the program
-    zf = zfile.ZipFile("received_files/secret.zip")
-    zf.extractall("received_files", pwd=pw)
