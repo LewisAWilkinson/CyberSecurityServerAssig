4d660d7a7e7eaf81e611cc1e05b94dff9479fb0a
Lewis <lewisawilkinson@outlook.com>
2017-11-29 23:58:46 +0000

    Calling all from main through calling tree

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import s5005662_Assignment_Server1.source_code.pwRetrieve as pWR
+import s5005662_Assignment_Server1.source_code.pwRetrieve as pWR  # Importing pwRetrieve
 
 pWR.selection(pWR.menu1_input())  # Calling the selection function using the returned value from menu1_input
 pWR.selection2(pWR.menu2_input())  # Calling the selection function using the returned value from menu2_input
