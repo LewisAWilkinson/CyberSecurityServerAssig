c38350ec4d8fc77cccb392b57d7dd1c0205caf9f
Lewis <lewisawilkinson@outlook.com>
2017-11-30 02:02:59 +0000

    Decoding text

--- s5005662_Assignment_Server2/source_code/decode.py
+++ s5005662_Assignment_Server2/source_code/decode.py
@@ -1,6 +1,14 @@
 import os
-import s5005662_Assignment_Server2.source_code.received_files.decode as dCR
+import received_files.decode as dC
 
 
 def decode_text():
+    decode_me_file = open("received_files/decode_me", "r")
+    coded_text = file.read()
+    decoded_text = dC.bin2utf(coded_text)
+    print decoded_text
+
+
+decode_text()
+
 
