4ac07edaf93d561e5369776fca6049c1c5e98c0d
Lewis <lewisawilkinson@outlook.com>
2017-11-30 00:24:42 +0000

    Calling all from main through calling tree

--- s5005662_Assignment_Server1/source_code/createHistogram.py
+++ s5005662_Assignment_Server1/source_code/createHistogram.py
@@ -1,10 +1,9 @@
-import os
 from matplotlib import pyplot as mpl
 import numpy as nmpy
 
 
 def create_histogram():  # Creating a histogram of data using the data.txt
-    y = nmpy.genfromtxt('received_files/data.txt')
+    y = nmpy.genfromtxt('s5005662_Assignment_Server1/received_files/data.txt')
     mpl.hist(y, 50, normed=1, facecolor="y", edgecolor="g")  # Cosmetic changes to the histogram
     mpl.xlabel("X-axis")  # Labelling the axis
     mpl.ylabel("Y-axis")  # ^
--- s5005662_Assignment_Server1/source_code/unzip.py
+++ s5005662_Assignment_Server1/source_code/unzip.py
@@ -3,4 +3,4 @@ import zipfile as zfile
 
 def unzip_secret(pw):  # Unzips the file secret.zip using the password found using the program
     zf = zfile.ZipFile("s5005662_Assignment_Server1/received_files/secret.zip")
-    zf.extractall("received_files", pwd=pw)
+    zf.extractall("s5005662_Assignment_Server1/received_files", pwd=pw)
